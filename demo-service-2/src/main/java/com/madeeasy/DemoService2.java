d0b4d690e5e79d1a5ba598df59576d3eb9967972
pabitra bera <pabitrabera2001@gmail.com>
2023-11-07 12:55:35 +0530

    first commit

---
+++ demo-service-2/src/main/java/com/madeeasy/DemoService2.java
@@ -0,0 +1,35 @@
+package com.madeeasy;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/department")
+@OpenAPIDefinition(
+        info = @Info(contact = @Contact(name = "pabitra bera",
+                email = "pabitrabera2001@gmail.com"), title = "Product Service",
+                termsOfService = "www.product.com/terms-and-condition",
+                description = "Product service provide the product information's to the other users.",
+                license = @License(name = "product service licence", url = "www.product.com/licence"),
+                version = "v1"))
+@EnableDiscoveryClient
+public class DemoService2 {
+    @GetMapping("/demo")
+    public String welcome() {
+        return "this is from demo-service-2";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoService2.class, args);
+        System.out.println("Hello world!");
+    }
+}
